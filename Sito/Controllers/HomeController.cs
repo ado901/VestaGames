39738a3b59599208114b597179b528a5d47640c4
ado901 <xadozz@gmail.com>
2021-05-13 16:51:48 +0200

    login + registrazione con wcf e chiamata a db

--- Sito/Controllers/HomeController.cs
+++ Sito/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sito.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,8 +7,10 @@ using System.Web.Mvc;
 
 namespace Sito.Controllers
 {
+    
     public class HomeController : Controller
     {
+        public static ServiceReference1.Service1Client wcf = new ServiceReference1.Service1Client();
         public ActionResult Index()
         {
             return View();
@@ -24,6 +27,50 @@ namespace Sito.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult Registrazione()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Registrazione(utenteRegistrato utente)
+        {
+            if (ModelState.IsValid)
+            {
+                var result= wcf.Registrazione(utente.ut);
+                if (result.Item1)
+                {
+                    Session["utenteAttivo"] = utente.ut;
+                    return View("Index");
+                }
+                
+            }
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(utenteLoggato utente)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = wcf.Login(utente.ut);
+                if (result.Item1)
+                {
+                    Session["utenteAttivo"] = utente.ut.email;
+                    return View("Index");
+                }
+
+                
+            }
             return View();
         }
     }
