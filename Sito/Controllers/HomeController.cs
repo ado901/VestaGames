986db0f17c827d6198494a48166852fc5d78abbd
Luco104 <andrelucato2303@gmail.com>
2021-06-18 17:23:39 +0200

    ORA VA SENZA MODELLO NON SO PERCHé MA VA (modificato view di prodotti, tolto about)

--- Sito/Controllers/HomeController.cs
+++ Sito/Controllers/HomeController.cs
@@ -17,12 +17,6 @@ namespace Sito.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
 
         public ActionResult Contact()
         {
